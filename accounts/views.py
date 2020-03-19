0570bb05c6a05d8ecb0ca1fedcb22645afb94622
lauratimm <laura.timm@ucalgary.ca>
2020-03-19 17:39:25 -0600

    March 19th 5:39

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup_view(request):
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #log the user in
+            return redirect('views.submission') #add redirect 'module:list'
+        #in articles urls
+        #urlpatterns=[
+        #   url('', views.article_list, name="list")
+
     form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
