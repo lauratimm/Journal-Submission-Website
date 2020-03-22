e7349f4f6821d62db90fc9d8c72655484a369f11
lauratimm <laura.timm@ucalgary.ca>
2020-03-22 11:31:03 -0600

    added comments

--- submission/urls.py
+++ submission/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'submission'
 
+#The `urlpatterns` list routes URLs to views, '^$' goes to the submission/ webpage, and '^logout/' routes to submission/logout
 urlpatterns = [
     url('^$', views.submission_view, name='submission'),
     url('^logout/', views.submissionLogout_view, name='submission')]
--- submission/views.py
+++ submission/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-
+#each of these are view requests that link the url pattern to the html webpage
 def submission_view(request):
     return render(request, 'authorHome.html')
 
