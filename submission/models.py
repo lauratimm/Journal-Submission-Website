23cc322d4f594db0c2fd2c93b5422888c286f157
velvetarchangel <32275484+velvetarchangel@users.noreply.github.com>
2020-03-29 18:53:18 -0600

    Update models.py

--- submission/models.py
+++ submission/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+#Create a model for submission
+
+class Submission(models.Model):
+  #defines all the possible status for the papers
+  SUBMITTED = 'SUB'
+  REVIEWED = 'REV'
+  REJECTED = 'REJ'
+  ACCEPTED = 'ACC'
+  
+  title = models.CharField()  #paper will have a title
+  slug = models.SlugField()   #paper will have a URL of somesort
+  file = models.FileField() #can also add a parameter to upload it to some where
+  
+  
+  #a dictionary object which maps the different status to strings
+  STATUS_CHOICES = [
+                   (SUBMITTED, 'Submitted'),
+                   (REVIEWED, 'Reviewed'),
+                   (ACCEPTED), 'Accepted',
+                   (REJECTED, 'Rejected'),
+                   ]
+  
+  #status of paper 
+  STATUS_OF_PAPER = models.CharField(
+    max_length = 3,
+    choices = STATUS_CHOICES,
+    default = SUBMITTED,
+  )
+  
+  # will return paper status
+  def getStatus(self):
+    return self.STATUS_OF_PAPER
+  
