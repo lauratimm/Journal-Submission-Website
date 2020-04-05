7548abfd74273d19952177c784b44be7ea1694ac
Anna Chaykovska <anna.chaykovska@ucalgary.ca>
2020-04-05 14:44:38 -0600

    deleted home.models.User

--- home/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("<h1> This is the homepage</h1>")
--- userPages/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserpagesConfig(AppConfig):
-    name = 'userPages'
