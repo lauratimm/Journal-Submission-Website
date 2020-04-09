c60d4f4cef2a7bf71b307eb9de7f400e8eef1047
annachaykovska <60158249+annachaykovska@users.noreply.github.com>
2020-04-09 16:02:11 -0600

    Implemented PDF link to proposal reviewer view

--- userPages/views.py
+++ userPages/views.py
@@ -68,6 +68,27 @@ class ProposalDetailView(generic.DetailView):
     template_name = 'reviewer/proposal_detail.html'
 
 
+def reviewer_pdf_view(request):
+    # Create a file-like buffer to receive PDF data.
+    buffer = io.BytesIO()
+
+    # Create the PDF object, using the buffer as its "file."
+    p = canvas.Canvas(buffer)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    p.drawString(100, 100, "Hello world.")
+
+    # Close the PDF object
+    p.showPage()
+    p.save()
+
+    # FileResponse sets the Content-Disposition header so that browsers
+    # present the option to save the file.
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename='proposal.author_file.url')
+
+
 # Source: https://data-flair.training/blogs/django-file-upload/
 # Author: Laura Timm
 # Date Created: March 29, 2020
