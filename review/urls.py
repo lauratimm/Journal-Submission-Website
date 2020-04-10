4cf1343bc7898368b86916022359507b1a4e1826
annachaykovska <60158249+annachaykovska@users.noreply.github.com>
2020-04-09 23:14:02 -0600

    deleted review app

--- review/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'review'
-
-#The `urlpatterns` list routes URLs to views, '^$' goes to the submission/ webpage, and '^logout/' routes to submission/logout
-urlpatterns = [
-    url('^$', views.review_view, name='review'),
-    url('^logout/', views.reviewLogout_view, name='review')]
