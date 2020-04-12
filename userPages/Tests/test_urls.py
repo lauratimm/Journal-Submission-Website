587531bb0839c9937d835ba65d56800c1003898d
velvetarchangel <h.dastidar@gmail.com>
2020-04-12 17:07:04 -0600

    website url testing in userPages working

--- userPages/Tests/test_urls.py
+++ userPages/Tests/test_urls.py
@@ -2,28 +2,36 @@ from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from userPages.views import author_profile, author_view_journals, create_profile, ProposalDetailView, reviewer_view_proposals
 
-
+# Author: Himika Dastidar
+# Date: 2020-04-12
+# Tutorial: https://www.youtube.com/watch?v=0MrgsYswT1c&list=PLbpAWbHbi5rMF2j5n6imm0enrSD9eQUaM&index=2
+# NOTE: EACH TEST CASE MUST BEGIN WITH TEST_*
 class TestUserPagesUrl(SimpleTestCase):
 
-    def proposal_view_is_resolved(self):
+    def test_proposal_view_is_resolved(self):
         url = reverse('Proposal View')
         self.assertEquals(resolve(url).func, reviewer_view_proposals)
 
-
-    def proposal_detail_is_resolved(self):
+    """
+    Currently failing test
+    """
+    def test_proposal_detail_is_resolved(self):
         url = reverse('proposal-detail')
         self.assertEquals(resolve(url).func, ProposalDetailView.as_view())
 
-    def create_profile_is_resolved(self):
+    """
+    Currently failing test
+    """
+    def test_create_profile_is_resolved(self):
         url = reverse('create-profile')
         self.assertEquals(resolve(url).func, create_profile)
 
 
-    def author_view_journals_is_resolved(self):
+    def test_author_view_journals_is_resolved(self):
         url = reverse('Journal View')
         self.assertEquals(resolve(url).func, author_view_journals)
 
 
-    def author_profile_is_resolved(self):
+    def test_author_profile_is_resolved(self):
         url = reverse('Author Profile')
         self.assertEquals(resolve(url).func, author_profile)
